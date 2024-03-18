ef111c8b5d2dbd2dff9ecba9f104027d170a5ed8
= <=>
2024-03-18 13:50:06 +0700

    devkh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4826188337648981"
+     crossorigin="anonymous"></script>
     @inertiaHead
 </head>
 
